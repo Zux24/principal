afac94a5e287ae50b77b33932987dcdadef541ef
Laura Rozas G <laurarozasg@gmail.com>
2020-02-09 01:10:03 -0300

    Add files via upload

---
+++ Python/creacionMatricesForma1.py
@@ -0,0 +1,13 @@
+# Definir variables:
+matriz = []
+nFilas = 5 
+nColumnas = 3
+# Crear la matriz:
+for f in range(nFilas):
+    matriz.append([])       
+    for c in range(nColumnas):
+        matriz[f].append(0)
+# Imprimir Matriz:
+for i in matriz:
+    print(i)
+
